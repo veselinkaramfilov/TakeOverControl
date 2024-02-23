d6974fb1c9b972fb4877257d2740a6fc4814ae70
Veselin Karamfilov <veselinyurievkaramfilov@gmail.com>
2024-02-23 22:20:39 +0000

    registration activity

--- app/src/main/java/com/example/takeovercontrol/SplashActivity.java
+++ app/src/main/java/com/example/takeovercontrol/SplashActivity.java
@@ -16,7 +16,8 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                startActivity(new Intent(SplashActivity.this,RegistrationActivity.class));
+                finish();
             }
         }, 1000);
 
