0c2d4055d2fbb4f9cc30859ec10d8b65d658354e
Veselin Karamfilov <veselinyurievkaramfilov@gmail.com>
2024-02-25 20:47:51 +0000

    add activity

--- app/src/main/java/com/example/takeovercontrol/AddActivity.java
+++ app/src/main/java/com/example/takeovercontrol/AddActivity.java
@@ -49,6 +49,39 @@ public class AddActivity extends AppCompatActivity {
                     case "Cider":
                         adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_cider_size, R.layout.spinner_layout);
                         break;
+                    case "Whiskey":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_whiskey_size, R.layout.spinner_layout);
+                        break;
+                    case "Wine":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_wine_size, R.layout.spinner_layout);
+                        break;
+                    case "Champagne":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_champagne_size, R.layout.spinner_layout);
+                        break;
+                    case "Vodka":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_vodka_size, R.layout.spinner_layout);
+                        break;
+                    case "Tequila":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_tequila_size, R.layout.spinner_layout);
+                        break;
+                    case "Cognac":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_cognac_size, R.layout.spinner_layout);
+                        break;
+                    case "Liqueur":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_liqueur_size, R.layout.spinner_layout);
+                        break;
+                    case "Cocktail":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_cocktail_size, R.layout.spinner_layout);
+                        break;
+                    case "Hot Drink":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_hot_drink_size, R.layout.spinner_layout);
+                        break;
+                    case "Other":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_other_size, R.layout.spinner_layout);
+                        break;
+                    case "Just Buy":
+                        adapterSize = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_just_buy_size, R.layout.spinner_layout);
+                        break;
                     default:
                         break;
                 }
@@ -68,13 +101,47 @@ public class AddActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedSize = spinnerSize.getSelectedItem().toString();
 
-                switch (selectedSize) {
-                    case "Select Size":
-                        adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_default_alcohol, R.layout.spinner_layout);
-                        break;
-                    default:
-                        // Handle other cases if needed
-                        break;
+                if(selectedSize.equals("Select Size")) {
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_default_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Beer")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_beer_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Cider")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_cider_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Whiskey")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_whiskey_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Wine")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_wine_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Champagne")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_champagne_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Vodka")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_vodka_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Tequila")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_tequila_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Cognac")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_cognac_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Liqueur")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_liqueur_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Cocktail")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_cocktail_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Hot Drink")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_hot_drink_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Other")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_other_alcohol, R.layout.spinner_layout);
+                }
+                else if(!selectedSize.equals("Select Size") && selectedType.equals("Just Buy")){
+                    adapterAlcohol = ArrayAdapter.createFromResource(parent.getContext(), R.array.array_just_buy_alcohol, R.layout.spinner_layout);
                 }
 
                 adapterAlcohol.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
