8ffa416e141f4e3fbae6276cc8e02ff56b5fa5eb
Veselin Karamfilov <veselinyurievkaramfilov@gmail.com>
2024-02-25 21:14:06 +0000

    main activity

--- app/src/main/java/com/example/takeovercontrol/MainActivity.java
+++ app/src/main/java/com/example/takeovercontrol/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.takeovercontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.button_add);
+        addButton.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this, AddActivity.class)));
+
     }
 }
